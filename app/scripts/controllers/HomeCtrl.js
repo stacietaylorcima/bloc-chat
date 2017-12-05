4741f9a600600dc20602e46507ee2037f1aef77f
stacietaylorcima <stacietaylorcima@gmail.com>
2017-12-05 13:10:23 -0800

    fix HomeCtrl dependancy Room and add ng-repeat to an li

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,9 +3,8 @@
 
 (function(){
   // Inject the Room service so that you can assign the array of objects retrieved by the all method to a $scope variable.
-  function HomeCtrl('Room'){
+  function HomeCtrl(Room){
     this.chatrooms = Room.all;
-    // $scope.chatrooms = Room.all; Maybe this is how you do a scope vaiable instead of above. 
 
   }
 
