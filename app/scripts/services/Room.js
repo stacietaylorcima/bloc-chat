ebf6adfb9c6567b7eebcdd914084fc2449794dce
stacietaylorcima <stacietaylorcima@gmail.com>
2017-12-06 10:15:09 -0800

    use the AngularFiremethod () inside a Room factory method add

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -6,6 +6,18 @@
 
     Room.all = rooms;
 
+// The Room service method add should take a room object as an argument.
+// This room object will need to be created outside of this service.
+// Where could you create the room object? What are the pros and cons of where you create that object?
+    Room.add = function(room){
+      var roomList = $firebaseArray(ref);
+      roomList.$add({ foo: "bar" }).then(function(ref) {
+          var id = ref.key;
+          console.log("added record with id " + id);
+          list.$indexFor(id); // returns location in the array
+        });
+    }
+
     return Room;
   }
 
