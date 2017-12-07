1a7a6ecd5d6a71a8c2b1ce01492c62beb1210ee1
stacietaylorcima <stacietaylorcima@gmail.com>
2017-12-06 19:26:14 -0800

    add form to modal where user can create new chatroom

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,17 +1,23 @@
 (function() {
-  // This controller (ModalCtrl) listes for instructions from the 'New Chatroom' button in the home.html template.
+  // This controller (ModalCtrl) listens for instructions from the 'New Chatroom' button in the home.html template.
   // Once the user clicks to open the modal from the view, this controller reaches out to the $uibModal service and calls the open() method, passing optional values to it in a JS object.
   function ModalCtrl($uibModal) {
     this.open = function() {
 
       // modalInstance uses the $uibModal service to open the modal and links the modal.html template to the ModalInstanceCtrl
       // Note from Carrie: in the future, we'll need to do something with this modalInstance variable in the ModalCtrl, but for now, we won't be doing anything with it.
-      var modalInstance = $uibModal.open({
-          templateUrl: '/templates/modal.html',
-          controller: 'ModalInstanceCtrl as modalInstance'
-      })
+      function ModalCtrl($uibModal, Room) {
+        this.open = function() {
+            var modalInstance = $uibModal.open({
+                templateUrl: '/templates/modal.html',
+                controller: 'ModalInstanceCtrl as modalInstance'
+            })
+            console.log('modal opened')
 
-      console.log('modal opened')
+            modalInstance.result.then(function(chatroom) {
+                Room.createNewRoom(room)
+                console.log(Room.all)
+            })
         }
     }
 
