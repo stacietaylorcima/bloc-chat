004e812e311fb344da8a15ad7c14d38302dde744
stacietaylorcima <stacietaylorcima@gmail.com>
2017-12-11 10:33:03 -0800

    write placeholders for message data on home.html, prepare Message.js to getRoomById, prepare HomeCtrl to recieve info from Message service

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,12 +1,18 @@
-(funtion(){
-  function Message($firebaseArray){
+(function() {
+  function Message($firebaseArray) {
     var Message = {};
-    var ref = firebase.database().ref().child('messages');
+    var ref = firebase.database().ref().child("messages");
+    var messages = $firebaseArray(ref);
+
+    Message.getByRoomId = function(roomId) {
+        // Filter the messages by their room ID so that HomeCtrl can display all of  a room's messages on the home.html using ng-repeat.
+    };
 
     return Message;
+    };
   }
 
   angular
     .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message])
+    .factory('Message', ['$firebaseArray', Message]);
 })();
