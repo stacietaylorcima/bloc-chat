d0d1a13ff1fb6db1064175521abd8a3eb33e8224
stacietaylorcima <stacietaylorcima@gmail.com>
2017-12-16 19:08:53 -0800

    update logic in BlocChatCookies.js and optimize the form details on login.html

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -2,6 +2,7 @@
   function LoginCtrl($uibModal, $uibModalInstance, $cookies) {
     this.ok = function(username) {
       $uibModalInstance.close(this.login);
+      // write and store the username in a cookie here. I think in this controller is where we will put the write cookie function, then we put the $cookieStore service in BlocChatCookies.js and call it from here? That's just based on an article I read. 
       console.log(username);
     }
   };
