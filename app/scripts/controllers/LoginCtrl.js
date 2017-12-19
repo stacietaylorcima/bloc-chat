94be876c0049ebb67bc400da55ce9bb819741062
stacietaylorcima <stacietaylorcima@gmail.com>
2017-12-18 19:28:16 -0800

    disable user from clicking submit if username field is empty, store username in a cookie on the browser

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -3,11 +3,11 @@
     this.ok = function(username) {
       console.log(username);
       $uibModalInstance.close(this.login);
-      // write and store the username in a cookie here. I think in this controller is where we will put the write cookie function, then we put the $cookieStore service in BlocChatCookies.js and call it from here? That's just based on an article I read.
+      $cookies.put("blocChatCurrentUser", username);
     }
   };
 
   angular
     .module('blocChat')
-    .controller('LoginCtrl', ['$uibModal', '$uibModalInstance', LoginCtrl])
+    .controller('LoginCtrl', ['$uibModal', '$uibModalInstance', '$cookies', LoginCtrl])
 })();
